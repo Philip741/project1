e36767d67e803fcc4aad6af2ac10091bcc303c38
Zmoore <zmoore371@gmail.com>
2021-09-11 13:35:07 -0400

    added more food items

--- assets/js/pg2.js
+++ assets/js/pg2.js
@@ -1,25 +1,30 @@
 var submitBtn = $('.submitBtn');
 var randomBtn = $('.randomBtn');
 
-submitBtn.on("click", function(){
+submitBtn.on("click", function () {
     var meat = [];
     var veg = [];
-    var allergy = []; 
+    var allergy = [];
 
-    $.each($("input[name='meat']:checked"), function(){
-     meat.push($(this).val());
+    $.each($("input[name='meat']:checked"), function () {
+        meat.push($(this).val());
     });
 
-   $.each($("input[name='veggie']:checked"), function(){
-    veg.push($(this).val());
+    $.each($("input[name='veggie']:checked"), function () {
+        veg.push($(this).val());
     });
 
-    $.each($("input[name='allergy']:checked"), function(){
-         allergy.push($(this).val());
+    $.each($("input[name='allergy']:checked"), function () {
+        allergy.push($(this).val());
     });
-        
-    console.log(meat)
-    console.log(veg)
-    console.log(allergy)
+
+    fetchApi(meat, veg, allergy)
 });
 
+function fetchApi (meat, veg, allergy) {
+    requestUrl = "https://www.themealdb.com/api/json/v2/9973533/filter.php?i=" //+ ingredients list 
+    console.log(meat)
+    console.log(veg)
+    console.log(allergy) 
+    
+}
