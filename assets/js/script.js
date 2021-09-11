497c740923fce78df99f37a6da2fafdcca069aa8
jl118 <jen.liebelt@gmail.com>
2021-09-11 11:10:26 -0400

    Added button functionality to page 3

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Page 3
+
+// adds event listener and location for the 'Back to Recipe' button
+$('#recipe-btn').on('click', function() {
+    location.href = "page2.html";
+});
+
+// adds event listener and location for the 'Return to Home' button
+$('#home-btn').on('click', function() {
+    location.href = "index.html";
+});
+
+
