278faed3bd76e9e4d6511446f7f5a1ee27cb4c2f
Zmoore <zmoore371@gmail.com>
2021-09-14 19:25:33 -0400

    takes user input and randomizes selection to one choice. adds to selected ingredients array and puts into request url. request url then calls for meal with selected ingredients

--- assets/js/pg2.js
+++ assets/js/pg2.js
@@ -6,6 +6,8 @@ submitBtn.on("click", function () {
     var veg = [];
     var allergy = [];
 
+    //fish options salmon 
+
     $.each($("input[name='meat']:checked"), function () {
         meat.push($(this).val());
     });
@@ -21,30 +23,60 @@ submitBtn.on("click", function () {
     fetchApi(meat, veg, allergy)
 });
 
-function fetchApi (meat, veg, allergy) {
+function fetchApi(meat, veg, allergy) {
     requestUrl = "https://www.themealdb.com/api/json/v2/9973533/filter.php?i=" //+ ingredients list 
     console.log(meat)
     console.log(veg)
-    console.log(allergy) 
+    console.log(allergy)
+    selectedIngredients = []
+
+    if (meat.length >= 1) {
+    selectedIngredients.push(meat)
+    meat = meat[Math.floor(Math.random() * meat.length)]
+    } 
+    if (veg.length >= 1) {
+    veg = veg[Math.floor(Math.random() * veg.length)]
+    selectedIngredients.push(veg)
+    }
+
+    console.log(selectedIngredients)
+
+    requestUrl = requestUrl + selectedIngredients[0] + "," + selectedIngredients[1]
+    console.log(requestUrl)
+    
+    fetch(requestUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    saveChoice(data);
+                })
+            }
+        }
+        )
+
 
 }
 
-randomBtn.on('click', function() {
+randomBtn.on('click', function () {
     requestUrl = "https://www.themealdb.com/api/json/v2/9973533/random.php"
 
     fetch(requestUrl)
-     .then(function (response) {
-         if(response.ok) {
-             response.json().then(function (data){
-                 saveRandom(data);
-             })
-         } else {
-             return //redirect to a 404 page
-         }
-     })
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    saveRandom(data);
+                })
+            } else {
+                return //redirect to a 404 page
+            }
+        })
 
 })
 
 function saveRandom(random) {
     console.log(random)
+}
+
+function saveChoice(choice) {
+    console.log(choice)
 }
